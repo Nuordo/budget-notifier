eb59678f0a4c5996c5d234aaf302bec8a46e12b5
Karol Sójko <karolsojko@gmail.com>
2017-07-20 12:10:48 +0200

    Fix schedule to Wednesday evenings

--- scripts/scheduler.js
+++ scripts/scheduler.js
@@ -2,5 +2,5 @@ import schedule from 'node-schedule'
 import { loadData } from './sheetsClient'
 
 export default () => {
-  schedule.scheduleJob('10 10 * * 4', loadData)
+  schedule.scheduleJob('0 20 * * 3', loadData)
 }
